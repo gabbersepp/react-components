52108ed7801aff76dd3261275b087432c72e26af
Josef Biehler <gabbersepp@googlemail.com>
2019-04-19 07:46:42 +0200

    add chk test

---
+++ src/ts/elements/Checkbox.test.tsx
@@ -0,0 +1,15 @@
+import { shallow } from "enzyme";
+import Checkbox from "./Checkbox";
+import * as React from "react";
+
+describe("Checkbox", () => {
+    const onChangeFn = jest.fn();
+    it("should be toggled on click", () => {
+        const wrapper = shallow(<Checkbox onStateChange={onChangeFn}/>);
+        wrapper.find("div").hasClass("chk-box");
+        expect(wrapper.find("div").hasClass("chk-box--checked")).not.toBeTruthy();
+        wrapper.simulate("click");
+        expect(wrapper.hasClass("chk-box--checked")).toBeTruthy();
+        expect(onChangeFn).toHaveBeenCalledWith(true);
+    })
+})

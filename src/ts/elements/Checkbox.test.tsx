f85ee478401b68dc8320bad7399d34573aa39414
Josef Biehler <gabbersepp@googlemail.com>
2019-04-19 10:55:09 +0200

    add checkbox and if test

--- src/ts/elements/Checkbox.test.tsx
+++ src/ts/elements/Checkbox.test.tsx
@@ -1,15 +1,42 @@
 import { shallow } from "enzyme";
 import Checkbox from "./Checkbox";
 import * as React from "react";
+import ICheckboxProps from "../interfaces/ICheckboxProps";
 
 describe("Checkbox", () => {
-    const onChangeFn = jest.fn();
+    let onChangeFn: jest.Mock;
+
+    beforeEach(() => {
+        onChangeFn = jest.fn();
+    })
+
     it("should be toggled on click", () => {
         const wrapper = shallow(<Checkbox onStateChange={onChangeFn}/>);
-        wrapper.find("div").hasClass("chk-box");
         expect(wrapper.find("div").hasClass("chk-box--checked")).not.toBeTruthy();
         wrapper.simulate("click");
-        expect(wrapper.hasClass("1chk-box--checked")).toBeTruthy();
+        expect(wrapper.hasClass("chk-box--checked")).toBeTruthy();
         expect(onChangeFn).toHaveBeenCalledWith(true);
+    })
+
+    it("should not be toggled if disabled", () => {
+        const wrapper = shallow(<Checkbox disabled={true} onStateChange={onChangeFn}/>);
+        wrapper.simulate("click");
+        expect(wrapper.hasClass("chk-box--checked")).not.toBeTruthy();
+        expect(onChangeFn).not.toHaveBeenCalled();
+    })
+
+    it("should update on props change", () => {
+        const wrapper = shallow<Checkbox, ICheckboxProps>(<Checkbox onStateChange={onChangeFn}/>);
+        wrapper.setProps({
+            disabled: true
+        });
+        expect(wrapper.hasClass("chk-box--disabled")).toBeTruthy();
+
+        wrapper.setProps({
+            disabled: false,
+            checked: true
+        });
+
+        expect(wrapper.hasClass("chk-box--checked")).toBeTruthy();
     })
 })

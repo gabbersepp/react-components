579bfa14943eb7e99008eb58ed82e9f495acb967
Josef Biehler <gabbersepp@googlemail.com>
2019-04-19 07:11:03 +0200

    add click test

--- src/ts/elements/Button.test.tsx
+++ src/ts/elements/Button.test.tsx
@@ -2,9 +2,16 @@ import { shallow } from 'enzyme'
 import Button from "./Button";
 import * as React from 'react';
 
-describe("Test2", () => {
-    it("asd", () => {
-        const wrapper = shallow(<Button title="asd" onClick={() => {}}/>)
-        expect(wrapper.text()).toBe("asd");
-    })
+describe("Button", () => {
+    it("should have title 'Test title'", () => {
+        const wrapper = shallow(<Button title="Test title" onClick={() => {}}/>)
+        expect(wrapper.text()).toBe("Test title");
+    });
+
+    it("should react to click", () => {
+        const clickFn = jest.fn();
+        const wrapper = shallow(<Button title="" onClick={clickFn}/>);
+        wrapper.find("div").simulate("click");
+        expect(clickFn).toHaveBeenCalledTimes(1);
+    });
 })

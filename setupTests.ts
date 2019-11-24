6d62886d4f50a62ae077f3d5e463bbffa3247893
Josef Biehler <gabbersepp@googlemail.com>
2019-11-25 00:12:53 +0100

    use TS for cypress

--- setupTests.ts
+++ setupTests.ts
@@ -1,6 +1,8 @@
 import * as Enzyme from 'enzyme'
 import * as Adapter from 'enzyme-adapter-react-16'
 
+(global as any).jestExpect = expect as any;
+
 Enzyme.configure({
   adapter: new Adapter(),
 })
